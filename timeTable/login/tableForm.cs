aa2fed6270691d773f92f824825b0525af50bf48
wonhaeng <lwh32012@naver.com>
2023-12-07 22:14:58 +0900

    add InitializeTimeTable

--- timeTable/login/tableForm.cs
+++ timeTable/login/tableForm.cs
@@ -15,6 +15,45 @@ namespace login
         public Form1()
         {
             InitializeComponent();
+            InitializeTimetable(); //시작할 때 같이 실행
+        }
+
+        private void InitializeTimetable()
+        {
+            Random random = new Random();
+            Color randomColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+            TimeTable.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            TimeTable.RowTemplate.Height = 20;
+
+            // 초기설정 datagridview <- 이게 표임
+            TimeTable.ColumnCount = 6; // 교시 열(0번 열은 교시 사용)
+            TimeTable.Rows.Add("교시", "월요일", "화요일", "수요일", "목요일", "금요일");
+            for (int i = 0; i < 6; i++)
+            {
+                TimeTable.RowTemplate.Height = 70;
+            }
+
+            // 요일 설정
+            TimeTable.Rows.Add("1교시");
+            TimeTable.Rows.Add("2교시");
+            TimeTable.Rows.Add("3교시");
+            TimeTable.Rows.Add("4교시");
+            TimeTable.Rows.Add("5교시");
+            TimeTable.Rows.Add("6교시");
+            TimeTable.Rows.Add("7교시");
+            TimeTable.Rows.Add("8교시");
+
+
+            // 요일과 교시 셀들 눌러서 못 바꾸게 하는 거임(2차원 배열)
+            for (int i = 0; i < TimeTable.Rows.Count; i++)
+            {
+                for (int j = 0; j < TimeTable.Columns.Count; j++)
+                {
+                    DataGridViewCell cell = TimeTable.Rows[i].Cells[j];
+                    cell.ReadOnly = true; // 읽을 수만 있게
+                }
+            }
+
         }
     }
 }
